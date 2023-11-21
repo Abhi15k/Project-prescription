cb2d35d7b5416176fa88c9588695a0e6fc9730bd
Srivatsajbhat <srivatsajbhat@gmail.com>
2023-11-21 11:01:09 +0530

    Updated admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,11 @@ import Prescription from './pages/homepage/user/Prescription';
 import Account from './pages/homepage/user/Account';
 import About from './pages/homepage/user/About';
 import AddCompany from './pages/homepage/admin/AddCompany';
+import AddPharmacy from './pages/homepage/admin/AddPharmacy';
+import ManagePharmacy from './pages/homepage/admin/ManagePharmacy';
+import AddState from './pages/homepage/admin/AddState';
+import AddDistrict from './pages/homepage/admin/AddDistrict';
+import AddCity from './pages/homepage/admin/AddCity';
 
 function App() {
   return (
@@ -28,6 +33,11 @@ function App() {
       <Route path='/dashbord' element={<AdminRoute />} >
         <Route path='admin' element={<AdminPage />} />
         <Route path='admin/addCompany' element={<AddCompany />} />
+        <Route path= 'admin/addPharmacy' element={< AddPharmacy />} />
+        <Route path= 'admin/managePharmacy' element={< ManagePharmacy />} />
+        <Route path= 'admin/addState' element={< AddState />} />
+        <Route path= 'admin/addDistrict' element={< AddDistrict />} />
+        <Route path= 'admin/addCity' element={< AddCity />} />
       </Route>
     </Routes>
 
